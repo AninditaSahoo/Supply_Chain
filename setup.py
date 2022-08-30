206c8ddd2438da23f8129866070a885552c4976f
AninditaSahoo <analyticsanindita@gmail.com>
2022-08-30 21:33:55 +0530

    Initial work has been done

---
+++ setup.py
@@ -0,0 +1,21 @@
+from gettext import install
+from setuptools import setup
+from typing import List
+
+
+def get_requirements_list()->List[str]:
+    '''
+    This function will going to return a list of strings which will contain name for all the libraries specified with requirements.txt
+    '''
+    with open("requirements.txt","r") as f:
+        return f.readlines().remove("-e .")
+
+setup(
+name="Shipping-cost-prediction",
+version="0.0.1",
+author="Anindita",
+description="This is cover type predictor setup file",
+packages=["Supply_Chain"],
+install_requires=get_requirements_list()
+)
+
