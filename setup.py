e3534d334225e42fda42ac4ed2392599fd502f3e
AninditaSahoo <analyticsanindita@gmail.com>
2022-08-31 13:28:46 +0530

    folder structure created

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ def get_requirements_list()->List[str]:
     This function will going to return a list of strings which will contain name for all the libraries specified with requirements.txt
     '''
     with open("requirements.txt","r") as f:
-        return f.readlines().remove("-e .")
+        return f.readlines()
 
 setup(
 name="Shipping-cost-prediction",
-version="0.0.1",
+version="0.0.2",
 author="Anindita",
 description="This is cover type predictor setup file",
 packages=["Supply_Chain"],
